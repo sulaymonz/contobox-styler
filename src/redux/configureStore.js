3bd452477bb81c68f2d8253ffda26006517caf21
sulaymonz <sulaymonz@gmail.com>
2021-12-02 02:18:30 +0500

    Using redux-logger instead of Redux DevTools

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
+import { createLogger } from "redux-logger/src";
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux devtools
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+  return createStore(rootReducer, initialState, applyMiddleware(thunk, logger));
 }
