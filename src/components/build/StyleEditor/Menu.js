199718a5cdc47acc6a21585a70571e8469e038eb
sulaymonz <sulaymonz@gmail.com>
2022-05-22 05:22:15 +0500

    Fixed styled-component warnings by moving them out of render method.

--- src/components/build/StyleEditor/Menu.js
+++ src/components/build/StyleEditor/Menu.js
@@ -4,6 +4,32 @@ import { Button } from "./LayoutComponents";
 import * as buildActions from "../../../redux/actions/buildActions";
 import { useSelector, useDispatch } from "react-redux";
 
+const MenuWrapper = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: ${(props) => (props.showMenu ? "0" : "-100%")};
+  background-color: ${(props) => props.backgroundColor};
+  padding-top: ${(props) => props.paddingTop};
+  box-sizing: border-box;
+`;
+
+const TabButton = styled(Button)`
+  position: relative;
+  left: calc(50vw - ${(props) => props.width} / 2);
+  margin-bottom: ${(props) => props.marginBottom};
+`;
+
+const ActiveTabButton = styled(TabButton)`
+  color: ${(props) => props.color};
+  border: ${(props) => props.border};
+  background-color: ${(props) => props.backgroundColor};
+  &:hover {
+    background-color: ${(props) => props.backgroundColor};
+  }
+`;
+
 const Menu = (props) => {
   const components = useSelector(
     (state) => state.build.components.componentsByIds
@@ -14,47 +40,21 @@ const Menu = (props) => {
 
   const { wrapper, tabButton, activeTabButton } = props;
 
-  const MenuWrapper = styled.div`
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    left: 0;
-    top: ${showMenu ? "0" : "-100%"};
-    background-color: ${(props) => props.backgroundColor};
-    padding-top: ${(props) => props.paddingTop};
-    box-sizing: border-box;
-  `;
-
-  const TabButton = styled(Button)`
-    position: relative;
-    left: calc(50vw - ${(props) => props.width} / 2);
-    margin-bottom: ${(props) => props.marginBottom};
-  `;
-
-  const ActiveTabButton = styled(TabButton)`
-    color: ${(props) => props.color};
-    border: ${(props) => props.border};
-    background-color: ${(props) => props.backgroundColor};
-    &:hover {
-      background-color: ${(props) => props.backgroundColor};
-    }
-  `;
-
   return (
-    <MenuWrapper {...wrapper}>
+    <MenuWrapper {...wrapper} showMenu={showMenu}>
       {Object.entries(components).map(([componentID, component]) =>
         componentID === view ? (
           <ActiveTabButton
             {...tabButton}
             {...activeTabButton}
-            key={component.id}
+            key={componentID}
           >
             {component.type}
           </ActiveTabButton>
         ) : (
           <TabButton
             {...tabButton}
-            key={component.id}
+            key={componentID}
             onClick={() => {
               dispatch(buildActions.styleEditorShowComponent(componentID));
             }}
